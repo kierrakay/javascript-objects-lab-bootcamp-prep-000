2075f8319d647396b50662ecd6a92f986337148c
merry-paradigm-9237 <k.merriman32@hotmail.com>
2019-06-28 14:39:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function deleteFromObjectByKey(object, key){
-
   var clone = Object.assign({}, object);
   delete clone[key];
   return clone;
