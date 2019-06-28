2180bba604f7483bb6e748321f154464b399b3f9
merry-paradigm-9237 <k.merriman32@hotmail.com>
2019-06-28 14:47:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,4 @@ function deleteFromObjectByKey(object, key){
  delete clone [key]
  return clone
 }
-function destructivelyDeleteFromObjectByKey(object, key){
- delete object[key]
- return object
-}
+
